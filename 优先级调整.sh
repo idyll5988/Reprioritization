c93dab687ffa14ee501cd1fc4599152997f9d83f
idyll <liushun968858@gmail.com>
2024-10-01 17:55:11 +0800

    Add files via upload
    Optimize the performance of recently used applications by adjusting their process priority, IO priority, and CPU core bindings to improve their performance in the system, turn off the screen and not running

--- 优先级调整.sh
+++ 优先级调整.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
 date="$( date "+%Y年%m月%d日%H时%M分%S秒")"
-#通过获取系统最近使用的应用进程并对该应用CPU频率、进程优先级、I/O优先级及CPU核心调整优先提高其性能、关屏不运行
+#优化最近使用的应用程序的性能，通过调整其进程的优先级、IO优先级以及CPU核心绑定，提高其在系统中的运行表现、关屏不运行
 while true; do
 screen_status=$(dumpsys window | grep "mScreenOn" | grep true)
 if [[ "${screen_status}" ]]; then
