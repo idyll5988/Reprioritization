b193a458987641ee8ba6f2e468c5f04b48e218fe
idyll <liushun968858@gmail.com>
2024-10-01 16:46:28 +0800

    Add files via upload
    By obtaining the most recently used application process on the system and adjusting the CPU frequency, process priority, I/O priority and CPU core of the application to prioritize the improvement of its performance, turn off the screen and do not run.

---
+++ 优先级调整.sh
@@ -0,0 +1,21 @@
+#!/system/bin/sh
+date="$( date "+%Y年%m月%d日%H时%M分%S秒")"
+#通过获取系统最近使用的应用进程并对该应用CPU频率、进程优先级、I/O优先级及CPU核心调整优先提高其性能、关屏不运行
+while true; do
+screen_status=$(dumpsys window | grep "mScreenOn" | grep true)
+if [[ "${screen_status}" ]]; then
+    namaapk=$(dumpsys activity recents | grep 'Recent #0:' | awk -F= '{print $2}' | awk '{print $1}')
+    p=$(pgrep -f "$namaapk" | head -n 1)
+	echo "$date *🔨- 已获取进程$namaapk并调整renice、ionice、taskset和chrt优先级*"
+    renice -n -20 -p $p
+    ionice -c 1 -n 0 -p $p 
+    taskset -c 0,1,2,3,4,5,6,7 -p $p
+    taskset -c 0-7 -p $p
+    taskset -c 0-7 $p
+    taskset -cp 0-7 $p
+    taskset -ap 00ff $p
+    chrt -f -p 1 $p 
+    chrt -fp 1 $p 
+fi
+sleep 60
+done
